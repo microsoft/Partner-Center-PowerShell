bbd3586550f1ab07452e10e3230aa71401916be2
Isaiah Williams <isaiah.williams@microsoft.com>
2019-05-12 07:58:36 -0500

    Moved the SDK to a separate repository

--- src/PartnerCenter/Models/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Microsoft.Store.PartnerCenter.PowerShell")]
