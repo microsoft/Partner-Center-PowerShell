89660341051a6b129d5a43494d467fca5d64cb39
Isaiah Williams <isaiah.williams@microsoft.com>
2019-09-17 17:21:24 -0500

    Adding the enforce MFA parameter and the ability to test the partner security requirements for a specific account

---
+++ src/PowerShell/Commands/TestPartnerSecurityRequirement.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Store.PartnerCenter.PowerShell.Commands
+{
+    using System.Management.Automation;
+    using Extensions;
+    using Identity.Client;
+    using IdentityModel.JsonWebTokens;
+    using Models.Authentication;
+    using System.Linq;
+    using System.Security.Claims;
+
+    [Cmdlet(VerbsDiagnostic.Test, "PartnerSecurityRequirement")]
+    public class TestPartnerSecurityRequirement : PSCmdlet
+    {
+        /// <summary>
+        /// The default application identifier value used when generating an access tokne.
+        /// </summary>
+        private const string PowerShellApplicationId = "04b07795-8ddb-461a-bbee-02f9e1bf7b46";
+
+        /// <summary>
+        /// Gets or sets the environment used for authentication.
+        /// </summary>
+        [Parameter(HelpMessage = "The environment use for authentication.", Mandatory = false)]
+        [Alias("EnvironmentName")]
+        [ValidateNotNullOrEmpty]
+        public EnvironmentName Environment { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag indicating if the device code flow should be used.
+        /// </summary>
+        [Alias("DeviceCode", "DeviceAuth", "Device")]
+        [Parameter(Mandatory = false, HelpMessage = "Use device code authentication instead of a browser control.")]
+        public SwitchParameter UseDeviceAuthentication { get; set; }
+
+        /// <summary>
+        /// Performs the execution of the command.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+            string result = "pass";
+
+            PartnerAccount account = new PartnerAccount
+            {
+                Tenant = "common",
+                Type = AccountType.User
+            };
+
+            PartnerEnvironment environment = PartnerEnvironment.PublicEnvironments[Environment];
+
+            if (UseDeviceAuthentication.IsPresent)
+            {
+                account.SetProperty("UseDeviceAuth", "true");
+            }
+            else
+            {
+                account.SetProperty("UseAuthCode", "true");
+            }
+
+            account.SetProperty(PartnerAccountPropertyType.ApplicationId, PowerShellApplicationId);
+
+            AuthenticationResult authResult = PartnerSession.Instance.AuthenticationFactory.Authenticate(
+                account,
+                environment,
+                new[] { $"{environment.PartnerCenterEndpoint}/user_impersonation" });
+
+
+            JsonWebToken jwt = new JsonWebToken(authResult.AccessToken);
+
+            WriteDebug("Checking if the access token contains the MFA claim...");
+
+            /*
+             * Obtain the authentication method reference (AMR) claim. This claim contains the methods used
+             * during authenitcation. See https://tools.ietf.org/html/rfc8176 for more information.
+             */
+
+            if (jwt.TryGetClaim("amr", out Claim claim))
+            {
+                if (!claim.Value.Contains("mfa"))
+                {
+                    WriteWarning("Unable to determine if the account authenticated using MFA.");
+                    result = "fail";
+                }
+            }
+            else 
+            {
+                WriteWarning("Unable to find the AMR claim, which means the ability to verify the MFA challenge happened will not be possible.");
+                result = "fail";
+            }
+
+            WriteObject(result);
+        }
+    }
+}
